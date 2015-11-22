f7b95ccfea2e3f0a03982564d6132462bdbe69ed
David Luecke <daff@neyeon.com>
2015-11-22 11:57:39 -0800

    All service tests passing

--- src/index.js
+++ src/index.js
@@ -1,37 +1,109 @@
-import Sequelize from 'sequelize';
+import Proto from 'uberproto';
+import filter from 'feathers-query-filters';
+import { types as errors } from 'feathers-errors';
 
-export { Sequelize };
+function getOrder(sort) {
+	let order = [];
 
-export class Service {
-  constructor(... args) {
-    this.sequelize = new Sequelize(... args);
-  }
+	Object.keys(sort).forEach(name =>
+		order.push([ name, sort[name] === 1 ? 'ASC' : 'DESC' ]));
 
-  find(params, callback) {
-
-  }
-
-  get(id, params, callback) {
-    this.Model.findById(id).then()
-  }
-
-  create(data, params, callback) {
-
-  }
-
-  update(id, data, params, callback) {
-
-  }
-
-  patch(id, data, params, callback) {
-
-  }
-
-  remove(id, params, callback) {
-
-  }
+	return order;
 }
 
-export default function(... args) {
-  return new Service(... args);
+function getWhere(query) {
+	let where = Object.assign({}, query);
+
+	Object.keys(where).forEach(prop => {
+		let value = where[prop];
+		if(value.$nin) {
+			value = Object.assign({}, value);
+
+			value.$notIn = value.$nin;
+			delete value.$nin;
+
+			where[prop] = value;
+		}
+	});
+
+	return where;
 }
+
+// Create the service.
+export const Service = Proto.extend({
+	init(Model) {
+    this.Model = Model;
+	},
+
+	find(params, callback) {
+		let where = getWhere(params.query);
+		let filters = filter(where);
+		let order = getOrder(filters.$sort || {});
+
+    this.Model.findAll({
+			where, order,
+			limit: filters.$limit,
+			offset: filters.$skip,
+			attributes: filters.$select || null
+		}).then(items => callback(null, items), callback);
+	},
+
+	get(id, params, callback) {
+		if(typeof id === 'function') {
+			return id(new errors.BadRequest('An id needs to be provided'));
+		}
+
+		this.Model.findById(id).then(instance => {
+			if(!instance) {
+				throw new errors.NotFound(`No record found for id '${id}'`);
+			}
+
+			return instance;
+		}).then(instance => callback(null, instance), callback);
+	},
+
+	create(data, params, callback) {
+		let promise = Array.isArray(data) ?
+			this.Model.bulkCreate(data) : this.Model.create(data);
+
+		promise.then(instance => callback(null, instance), callback);
+	},
+
+	patch(id, data, params, callback) {
+		this.Model.findById(id).then(instance => {
+			if(!instance) {
+				throw new errors.NotFound(`No record found for id '${id}'`);
+			}
+			return instance.update(data);
+		}).then(data => callback(null, data), callback);
+	},
+
+	update(id, data, params, callback) {
+		this.Model.findById(id).then(instance => {
+			if(!instance) {
+				throw new errors.NotFound(`No record found for id '${id}'`);
+			}
+
+			let copy = {};
+			Object.keys(instance.toJSON()).forEach(key => {
+				if(typeof data[key] === 'undefined') {
+					copy[key] = null;
+				} else {
+					copy[key] = data[key];
+				}
+			});
+
+			return instance.update(copy);
+		}).then(data => callback(null, data), callback);
+	},
+
+	remove(id, params, callback) {
+		this.Model.findById(id).then(instance => {
+			return instance.destroy().then(() => instance);
+		}).then(data => callback(null, data), callback);
+	}
+});
+
+export default module.exports = function() {
+  return Proto.create.apply(Service, arguments);
+};
--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,40 @@
-import assert from 'assert';
-import plugin from '../src';
+// jshint expr:true
 
-describe('feathers-sequelize', () => {
-  it('basic functionality', done => {
-    assert.equal(typeof plugin, 'function', 'It worked');
-    done();
+import baseTests from 'feathers-service-tests';
+import Sequelize from 'sequelize';
+import errors from 'feathers-errors';
+import service from '../src';
+
+describe('Feathers Sequelize Service', () => {
+  const sequelize = new Sequelize('sequelize', null, null, {
+    dialect: 'sqlite',
+    storage: './db.sqlite',
+    logging: false
   });
+  const Person = sequelize.define('user', {
+    name: {
+      type: Sequelize.STRING
+    },
+    age: {
+      type: Sequelize.INTEGER
+    }
+  }, {
+    freezeTableName: true
+  });
+  const _ids = {};
+  const people = service(Person);
+
+  beforeEach(done => {
+    Person.sync({ force: true }).then(() => {
+      return Person.create({
+        name: 'Doug',
+        age: 32
+      });
+    }).then(user => {
+      _ids.Doug = user.id;
+      done();
+    });
+  });
+
+  baseTests(people, _ids, errors.types);
 });
