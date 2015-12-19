c09fa12c423f8605146676ec91fea662826d9540
Eric Kryski <e.kryski@gmail.com>
2015-12-18 23:22:16 -0700

    spaces > tabs

--- src/index.js
+++ src/index.js
@@ -6,124 +6,124 @@ import errors from 'feathers-errors';
 import {errorHandler, getOrder, getWhere} from './utils';
 
 class Service {
-	constructor(options) {
-		this.paginate = options.paginate || {};
+  constructor(options) {
+    this.paginate = options.paginate || {};
     this.Model = options.Model;
-		this.id = options.id || 'id';
-	}
+    this.id = options.id || 'id';
+  }
 
-	extend(obj) {
-		return Proto.extend(obj, this);
-	}
+  extend(obj) {
+    return Proto.extend(obj, this);
+  }
 
-	find(params) {
-		let where = getWhere(params.query);
-		let filters = filter(where);
-		let order = getOrder(filters.$sort || {});
-		let query = {
-			where, order,
-			limit: filters.$limit,
-			offset: filters.$skip,
-			attributes: filters.$select || null
-		};
+  find(params) {
+    let where = getWhere(params.query);
+    let filters = filter(where);
+    let order = getOrder(filters.$sort || {});
+    let query = {
+      where, order,
+      limit: filters.$limit,
+      offset: filters.$skip,
+      attributes: filters.$select || null
+    };
 
-		if (this.paginate.default) {
-			const limit = Math.min(filters.$limit || this.paginate.default,
-				this.paginate.max || Number.MAX_VALUE);
+    if (this.paginate.default) {
+      const limit = Math.min(filters.$limit || this.paginate.default,
+        this.paginate.max || Number.MAX_VALUE);
 
-			query.limit = limit;
+      query.limit = limit;
 
-			return this.Model.findAndCount(query).then(result => {
-				return {
-					total: result.count,
-					limit,
-					skip: filters.$skip || 0,
-					data: result.rows
-				};
-			}).catch(errorHandler);
-		}
+      return this.Model.findAndCount(query).then(result => {
+        return {
+          total: result.count,
+          limit,
+          skip: filters.$skip || 0,
+          data: result.rows
+        };
+      }).catch(errorHandler);
+    }
 
-		return this.Model.findAll(query).catch(errorHandler);
-	}
+    return this.Model.findAll(query).catch(errorHandler);
+  }
 
-	get(id) {
-		return this.Model.findById(id).then(instance => {
-			if(!instance) {
-				throw new errors.NotFound(`No record found for id '${id}'`);
-			}
+  get(id) {
+    return this.Model.findById(id).then(instance => {
+      if(!instance) {
+        throw new errors.NotFound(`No record found for id '${id}'`);
+      }
 
-			return instance;
-		})
-		.catch(errorHandler);
-	}
+      return instance;
+    })
+    .catch(errorHandler);
+  }
 
-	create(data) {
-		if (Array.isArray(data)) {
-			return this.Model.bulkCreate(data).catch(errorHandler);
-		}
+  create(data) {
+    if (Array.isArray(data)) {
+      return this.Model.bulkCreate(data).catch(errorHandler);
+    }
 
-		return this.Model.create(data).catch(errorHandler);
-	}
+    return this.Model.create(data).catch(errorHandler);
+  }
 
-	patch(id, data, params) {
-		const where = Object.assign({}, params.query);
+  patch(id, data, params) {
+    const where = Object.assign({}, params.query);
 
-		if(id !== null) {
-			where[this.id] = id;
-		}
+    if(id !== null) {
+      where[this.id] = id;
+    }
 
-		delete data[this.id];
+    delete data[this.id];
 
-		return this.Model.update(data, { where }).then(() => {
-			if(id === null) {
-				return this.find(params);
-			}
+    return this.Model.update(data, { where }).then(() => {
+      if(id === null) {
+        return this.find(params);
+      }
 
-			return this.get(id, params);
-		})
-		.catch(errorHandler);
-	}
+      return this.get(id, params);
+    })
+    .catch(errorHandler);
+  }
 
-	update(id, data) {
-		if(Array.isArray(data)) {
-			return Promise.reject('Not replacing multiple records. Did you mean `patch`?');
-		}
+  update(id, data) {
+    if(Array.isArray(data)) {
+      return Promise.reject('Not replacing multiple records. Did you mean `patch`?');
+    }
 
-		delete data[this.id];
+    delete data[this.id];
 
-		return this.Model.findById(id).then(instance => {
-			if(!instance) {
-				throw new errors.NotFound(`No record found for id '${id}'`);
-			}
+    return this.Model.findById(id).then(instance => {
+      if(!instance) {
+        throw new errors.NotFound(`No record found for id '${id}'`);
+      }
 
-			let copy = {};
-			Object.keys(instance.toJSON()).forEach(key => {
-				if(typeof data[key] === 'undefined') {
-					copy[key] = null;
-				} else {
-					copy[key] = data[key];
-				}
-			});
+      let copy = {};
+      Object.keys(instance.toJSON()).forEach(key => {
+        if(typeof data[key] === 'undefined') {
+          copy[key] = null;
+        } else {
+          copy[key] = data[key];
+        }
+      });
 
-			return instance.update(copy);
-		})
-		.catch(errorHandler);
-	}
+      return instance.update(copy);
+    })
+    .catch(errorHandler);
+  }
 
-	remove(id, params) {
-		const promise = id === null ? this.find(params) : this.get(id);
+  remove(id, params) {
+    const promise = id === null ? this.find(params) : this.get(id);
 
-		return promise.then(data => {
-			const where = Object.assign({}, params.query);
+    return promise.then(data => {
+      const where = Object.assign({}, params.query);
 
-			if(id !== null) {
-				where.id = id;
-			}
+      if(id !== null) {
+        where.id = id;
+      }
 
-			return this.Model.destroy({ where }).then(() => data);
-		})
-		.catch(errorHandler);
-	}
+      return this.Model.destroy({ where }).then(() => data);
+    })
+    .catch(errorHandler);
+  }
 }
 
 export default function init(Model) {
