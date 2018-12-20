61701136a4c79361b2ca5705f8bcd4b9bc4bde3d
David Luecke <daff@neyeon.com>
2018-12-20 11:30:42 -0800

    Migrate tests to async/await (#254)

--- test/hooks.hydrate.test.js
+++ test/hooks.hydrate.test.js
@@ -41,57 +41,57 @@ describe('Feathers Sequelize Hydrate Hook', () => {
     expect(hook).to.throw(Error);
   });
 
-  it('hydrates results for find()', () => {
-    return callHook(BlogPost, 'find', [{title: 'David'}]).then(hook =>
-      expect(hook.result[0] instanceof BlogPost).to.be.ok
-    );
+  it('hydrates results for find()', async () => {
+    const hook = await callHook(BlogPost, 'find', [{title: 'David'}]);
+
+    expect(hook.result[0] instanceof BlogPost);
   });
 
-  it('hydrates results for paginated find()', () => {
-    return callHook(BlogPost, 'find', {
+  it('hydrates results for paginated find()', async () => {
+    const hook = await callHook(BlogPost, 'find', {
       data: [{title: 'David'}]
-    }).then(hook =>
-      expect(hook.result.data[0] instanceof BlogPost).to.be.ok
-    );
+    });
+
+    expect(hook.result.data[0] instanceof BlogPost);
   });
 
-  it('hydrates results for get()', () => {
-    return callHook(BlogPost, 'get', {title: 'David'}).then(hook =>
-      expect(hook.result instanceof BlogPost).to.be.ok
-    );
+  it('hydrates results for get()', async () => {
+    const hook = await callHook(BlogPost, 'get', {title: 'David'});
+
+    expect(hook.result instanceof BlogPost);
   });
 
   ['create', 'update', 'patch'].forEach(method => {
-    it(`hydrates results for single ${method}()`, () => {
-      return callHook(BlogPost, method, {title: 'David'}).then(hook =>
-        expect(hook.result instanceof BlogPost).to.be.ok
-      );
+    it(`hydrates results for single ${method}()`, async () => {
+      const hook = await callHook(BlogPost, method, {title: 'David'});
+
+      expect(hook.result instanceof BlogPost);
     });
   });
 
   ['create', 'patch'].forEach(method => {
-    it(`hydrates results for bulk ${method}()`, () => {
-      return callHook(BlogPost, method, [{title: 'David'}]).then(hook =>
-        expect(hook.result[0] instanceof BlogPost).to.be.ok
-      );
+    it(`hydrates results for bulk ${method}()`, async () => {
+      const hook = await callHook(BlogPost, method, [{title: 'David'}]);
+
+      expect(hook.result[0] instanceof BlogPost);
     });
   });
 
-  it('hydrates included (associated) models', () => {
-    return callHook(BlogPost, 'get', {
+  it('hydrates included (associated) models', async () => {
+    const hook = await callHook(BlogPost, 'get', {
       title: 'David',
       comments: [{ text: 'Comment text' }]
     }, {
       include: [Comment]
-    }).then(hook =>
-      expect(hook.result.comments[0] instanceof Comment).to.be.ok
-    );
+    });
+
+    expect(hook.result.comments[0] instanceof Comment);
   });
 
-  it('does not hydrate if data is a Model instance', () => {
+  it('does not hydrate if data is a Model instance', async () => {
     const instance = BlogPost.build({ title: 'David' });
-    return callHook(BlogPost, 'get', instance).then(hook =>
-      expect(hook.result).to.equal(instance)
-    );
+    const hook = await callHook(BlogPost, 'get', instance);
+
+    expect(hook.result).to.equal(instance);
   });
 });
