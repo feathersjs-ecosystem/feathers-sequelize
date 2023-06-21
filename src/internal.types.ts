68355480c1093500520de5ae01491290974b5dcc
fratzinger <22286818+fratzinger@users.noreply.github.com>
2023-06-21 15:17:36 +0200

    fix: infer Paginated/Array from params

---
+++ src/internal.types.ts
@@ -0,0 +1,7 @@
+import type { Paginated, PaginationOptions } from '@feathersjs/feathers';
+
+export type PaginatedOrArray<R, P = undefined> = P extends { paginate: false }
+  ? R[]
+  : P extends { paginate: PaginationOptions }
+    ? Paginated<R>
+    : (Paginated<R> | R[]);
